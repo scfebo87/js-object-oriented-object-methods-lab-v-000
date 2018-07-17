4bf1f11d5b347a743a530a146c275e30ae39d2f0
Sean Christopher Febo <scf@techie.com>
2018-07-17 23:06:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ function BoardMember(name, homeState, training) {
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-  this.veto = function { 
+  this.veto = function() { 
     console.log(`No, I must disagree`);
   };
-  this.approve = function { 
+  this.approve = function() { 
     console.log(`You can do that!`);
   };
-  this.doCharity = function { 
+  this.doCharity = function() { 
     console.log(`I like to help people`);
   };
-  this.releasePressStatement = function { 
+  this.releasePressStatement = function() { 
     console.log(`You will see great things from Scuber.`);
   };
-  this.sayHi = function { 
+  this.sayHi = function() { 
     console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.homeState}.`);
   };
 }
